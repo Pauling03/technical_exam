13b8a02631df87b5f099bb2bdc19bc65a6c6b0b3
Paul Henry Elizalde <Paul Henry Elizalde>
2025-02-15 00:24:48 +0800

    add product seeder

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,6 +16,7 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
+        Product::factory()->count(50)->create();
         User::factory()->create([
             'name' => 'Paul Henry',
             'username' => 'Paully',
